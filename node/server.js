028788356df00d0c6b18294fd6db911b3ba7980d
Tommy Han <tommy@Tommys-Air.home>
2020-04-24 09:02:33 -0400

    adding different http verbs

--- node/server.js
+++ node/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.get('/profile', (req, res) =>{
+    const user = {
+        name: 'Sally',
+        hobby: 'soccer'
+    }
+    res.send(user);
+})
 app.get('/', (req, res) =>{
+    res.send("getting profile");
+})
+app.post('/profile', (req, res) =>{
     const user = {
         name: 'Sally',
         hobby: 'soccer'
