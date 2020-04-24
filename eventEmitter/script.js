5fb8dcd3156b0e6b23ee84719d8e151bb974e8f5
Tommy Han <tommy@Tommys-Air.home>
2020-04-24 18:19:53 -0400

    save changes

---
+++ eventEmitter/script.js
@@ -0,0 +1,23 @@
+var obj = {
+    greet: 'Hello'
+}
+
+console.log(obj.greet);
+console.log(obj['greet'])
+var prop = 'greet';
+console.log(obj[prop]);
+
+// functions and arrays
+var arr = [];
+
+arr.push(function(){
+    console.log('Hello World 1');
+});
+
+arr.push(function(){
+    console.log('Hello World 2');
+});
+
+arr.push(function(){
+    console.log('Hello World 3');
+});
