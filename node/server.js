93764036817df2d1b53e545f81504b7d62acd5b3
Tommy Han <tommy@Tommys-Air.home>
2020-04-24 08:47:11 -0400

    return JSON

--- node/server.js
+++ node/server.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((request, response) =>{
-    console.log('headers: ',request.headers)
     console.log('methods: ',request.method)
     console.log('url: ', request.url)
 
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<h1>Helllooooo</h1>')
+    const user = {
+        name: 'John',
+        hobby: 'Skating'
+    }
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
 
 })
 
