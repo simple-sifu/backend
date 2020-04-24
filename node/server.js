342ef05f550ce6dbce4575a9d706157da3dcf279
Tommy Han <tommy@Tommys-Air.home>
2020-04-24 08:43:30 -0400

    add logging

--- node/server.js
+++ node/server.js
@@ -1,6 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((request, response) =>{
+    console.log('headers: ',request.headers)
+    console.log('methods: ',request.method)
+    console.log('url: ', request.url)
+
     response.setHeader('Content-Type', 'text/html');
     response.end('<h1>Helllooooo</h1>')
 
