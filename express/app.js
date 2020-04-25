2c820b06986dc3eaa0efcda6917207255d40a124
Tommy Han <tommy@Tommys-Air.home>
2020-04-24 23:10:18 -0400

    return the style sheet

--- express/app.js
+++ express/app.js
@@ -3,8 +3,11 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-    res.send('<html><head></head><bod><h1>Hello World!</h1></body></html>')
+    res.send('<html><head><link href="assets/style.css" type="text/css" rel="stylesheet"/></head>' + 
+             '<body><h1>Hello World!</h1></body></html>')
 });
 
 app.get('/person/:id', function(req, res){
